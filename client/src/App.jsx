5d587142e0fe676c9ef27cb2beb3abb230893506
Mahendra Dani <danimahendra0904@gmail.com>
2023-06-12 19:58:56 +0530

    feat: init client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-5xl font-bold">VroomVerse Website</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
